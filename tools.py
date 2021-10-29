7801dfa01289f7178bccb85e4cd3508a950497cc
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2021-10-29 17:33:55 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,27 +5,22 @@ from copy import deepcopy
 
 def tensor(pattern):
     k = len(pattern)
-
     if k > 2:
         p = [tensor(pattern[0:int(k/2)]), tensor(pattern[int(k/2):k])]
     else:
         p = pattern
-
     return p
 
 def getComb(input, l):
     comb = list(iter.combinations(input, l))
     inter = []
-
     for c in comb:
         inter.append(list(c))
-
     comb = inter
-
+    
     if l == 1:
         res = [c[0] for c in comb]
         comb = res
-
     return comb
 
 def partition(collection):
@@ -43,30 +38,23 @@ def partition(collection):
 
 def getSubSet(list):
     output = []
-
     for p in partition(list):
         output.append(p)
-
     return output
 
 def removeIdx(input):
     output = []
-
     if not isinstance(input, list):
         output = input[0]
-
     else:
         for i in input:
             output.append(i[0])
-
     return output
 
 def getIdx(input, set):
     idx = []
-
     for i in input:
         idx.append(set.index(i))
-
     return idx
 
 def ordering(input, dict):
@@ -82,13 +70,10 @@ def lexicalOrder(decomp, dict):
                 for j in range(i+1, len(d)):
                     if ord(dict[d[i]][0]) > ord(dict[d[j]][0]):
                         d[i], d[j] = d[j], d[i]
-
     return decomp
 
 def Union(v, lst):
     output = set([])
-
     for i in v:
         output = output.union(set(lst[i]))
-
     return list(output)
